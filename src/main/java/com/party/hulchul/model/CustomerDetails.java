20fd493e92c08ad33aeb1f96b87d580ff587a67f
Soumya <sbasak>
2017-01-21 16:34:13 -0700

    Model classes created based on the json

---
+++ src/main/java/com/party/hulchul/model/CustomerDetails.java
@@ -0,0 +1,210 @@
+package com.party.hulchul.model;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+import javax.validation.Valid;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+/**
+* <h1>This class belongs to Customer Details related information</h1>
+*
+* @author  Soumya
+* @version 1.0
+* @since   2017-01-21
+*/
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({ "id", "firstName", "lastName", "emailId", "phone", "billingAddress", "postalAddress",
+		"notification" })
+public class CustomerDetails implements Serializable {
+
+	@JsonProperty("id")
+	private String id;
+	@JsonProperty("firstName")
+	private String firstName;
+	@JsonProperty("lastName")
+	private String lastName;
+	@JsonProperty("emailId")
+	private String emailId;
+	@JsonProperty("phone")
+	@Valid
+	private Phone phone;
+	@JsonProperty("billingAddress")
+	@Valid
+	private BillingAddress billingAddress;
+	@JsonProperty("postalAddress")
+	@Valid
+	private PostalAddress postalAddress;
+	@JsonProperty("notification")
+	private String notification;
+	@JsonIgnore
+	@Valid
+	private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+	private final static long serialVersionUID = 1198120642480536004L;
+
+	/**
+	 * No args constructor for use in serialization
+	 * 
+	 * @author Soumya
+	 * @version 1.0
+	 * @since 2017-01-21
+	 * 
+	 */
+	public CustomerDetails() {
+	}
+
+	/**
+	 * 
+	 * @author Soumya
+	 * @version 1.0
+	 * @since 2017-01-21
+	 * 
+	 * @param id
+	 * @param lastName
+	 * @param phone
+	 * @param emailId
+	 * @param notification
+	 * @param postalAddress
+	 * @param billingAddress
+	 * @param firstName
+	 */
+	public CustomerDetails(String id, String firstName, String lastName, String emailId, Phone phone,
+			BillingAddress billingAddress, PostalAddress postalAddress, String notification) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.emailId = emailId;
+		this.phone = phone;
+		this.billingAddress = billingAddress;
+		this.postalAddress = postalAddress;
+		this.notification = notification;
+	}
+
+	@JsonProperty("id")
+	public String getId() {
+		return id;
+	}
+
+	@JsonProperty("id")
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	@JsonProperty("firstName")
+	public String getFirstName() {
+		return firstName;
+	}
+
+	@JsonProperty("firstName")
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	@JsonProperty("lastName")
+	public String getLastName() {
+		return lastName;
+	}
+
+	@JsonProperty("lastName")
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	@JsonProperty("emailId")
+	public String getEmailId() {
+		return emailId;
+	}
+
+	@JsonProperty("emailId")
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	@JsonProperty("phone")
+	public Phone getPhone() {
+		return phone;
+	}
+
+	@JsonProperty("phone")
+	public void setPhone(Phone phone) {
+		this.phone = phone;
+	}
+
+	@JsonProperty("billingAddress")
+	public BillingAddress getBillingAddress() {
+		return billingAddress;
+	}
+
+	@JsonProperty("billingAddress")
+	public void setBillingAddress(BillingAddress billingAddress) {
+		this.billingAddress = billingAddress;
+	}
+
+	@JsonProperty("postalAddress")
+	public PostalAddress getPostalAddress() {
+		return postalAddress;
+	}
+
+	@JsonProperty("postalAddress")
+	public void setPostalAddress(PostalAddress postalAddress) {
+		this.postalAddress = postalAddress;
+	}
+
+	@JsonProperty("notification")
+	public String getNotification() {
+		return notification;
+	}
+
+	@JsonProperty("notification")
+	public void setNotification(String notification) {
+		this.notification = notification;
+	}
+
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this);
+	}
+
+	@JsonAnyGetter
+	public Map<String, Object> getAdditionalProperties() {
+		return this.additionalProperties;
+	}
+
+	@JsonAnySetter
+	public void setAdditionalProperty(String name, Object value) {
+		this.additionalProperties.put(name, value);
+	}
+
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder().append(id).append(firstName).append(lastName).append(emailId).append(phone)
+				.append(billingAddress).append(postalAddress).append(notification).append(additionalProperties)
+				.toHashCode();
+	}
+
+	@Override
+	public boolean equals(Object other) {
+		if (other == this) {
+			return true;
+		}
+		if ((other instanceof CustomerDetails) == false) {
+			return false;
+		}
+		CustomerDetails rhs = ((CustomerDetails) other);
+		return new EqualsBuilder().append(id, rhs.id).append(firstName, rhs.firstName).append(lastName, rhs.lastName)
+				.append(emailId, rhs.emailId).append(phone, rhs.phone).append(billingAddress, rhs.billingAddress)
+				.append(postalAddress, rhs.postalAddress).append(notification, rhs.notification)
+				.append(additionalProperties, rhs.additionalProperties).isEquals();
+	}
+
+}
---
+++ src/main/java/com/party/hulchul/model/Phone.java
@@ -0,0 +1,128 @@
+package com.party.hulchul.model;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+import javax.validation.Valid;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+/**
+* <h1>This class belongs to Phone related information</h1>
+*
+* @author  Soumya
+* @version 1.0
+* @since   2017-01-21
+*/
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({ "mobile", "home", "other" })
+public class Phone implements Serializable {
+
+	@JsonProperty("mobile")
+	private String mobile;
+	@JsonProperty("home")
+	private String home;
+	@JsonProperty("other")
+	private String other;
+	@JsonIgnore
+	@Valid
+	private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+	private final static long serialVersionUID = 6108658458171514104L;
+
+	/**
+	 * No args constructor for use in serialization
+	 * 
+	 * @author Soumya
+	 * @version 1.0
+	 * @since 2017-01-21
+	 * 
+	 */
+	public Phone() {
+	}
+
+	/**
+	 * 
+	 * @param home
+	 * @param other
+	 * @param mobile
+	 */
+	public Phone(String mobile, String home, String other) {
+		super();
+		this.mobile = mobile;
+		this.home = home;
+		this.other = other;
+	}
+
+	@JsonProperty("mobile")
+	public String getMobile() {
+		return mobile;
+	}
+
+	@JsonProperty("mobile")
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	@JsonProperty("home")
+	public String getHome() {
+		return home;
+	}
+
+	@JsonProperty("home")
+	public void setHome(String home) {
+		this.home = home;
+	}
+
+	@JsonProperty("other")
+	public String getOther() {
+		return other;
+	}
+
+	@JsonProperty("other")
+	public void setOther(String other) {
+		this.other = other;
+	}
+
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this);
+	}
+
+	@JsonAnyGetter
+	public Map<String, Object> getAdditionalProperties() {
+		return this.additionalProperties;
+	}
+
+	@JsonAnySetter
+	public void setAdditionalProperty(String name, Object value) {
+		this.additionalProperties.put(name, value);
+	}
+
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder().append(mobile).append(home).append(other).append(additionalProperties)
+				.toHashCode();
+	}
+
+	@Override
+	public boolean equals(Object other) {
+		if (other == this) {
+			return true;
+		}
+		if ((other instanceof Phone) == false) {
+			return false;
+		}
+		Phone rhs = ((Phone) other);
+		return new EqualsBuilder().append(mobile, rhs.mobile).append(home, rhs.home).append(other, rhs.other)
+				.append(additionalProperties, rhs.additionalProperties).isEquals();
+	}
+
+}
