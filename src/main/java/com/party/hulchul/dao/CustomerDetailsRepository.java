d5cec8c5e9f19b5bae227a588d3c8b31da37d8b9
Soumya <sbasak>
2017-01-22 04:34:57 -0700

    CRUD implementation has been committed.

--- src/main/java/com/party/hulchul/dao/CustomerDetailsRepository.java
+++ src/main/java/com/party/hulchul/dao/CustomerDetailsRepository.java
@@ -7,22 +7,48 @@ import org.springframework.data.repository.Repository;
 
 import com.party.hulchul.model.CustomerDetails;
 
-interface CustomerDetailsRepository extends Repository<CustomerDetails, String> {
+/**
+* <h1>The responsibility of this interface is to provide a common 
+* facade for all the CRUD operations related to Customer Details.</h1>
+*
+* @author  Soumya
+* @version 1.0
+* @since   2017-01-21
+*/
+
+public interface CustomerDetailsRepository extends Repository<CustomerDetails, String> {
 	
-	/**
-	* <h1>The responsibility of this interface is to provide a common 
-	* facade for all the CRUD operations related to Customer Details.</h1>
-	*
-	* @author  Soumya
-	* @version 1.0
-	* @since   2017-01-21
-	*/
-	
+    /**
+	 * @author  Soumya
+	 * @since   2017-01-22
+	 * 
+     * @param deleted
+     */
     void delete(CustomerDetails deleted);
     
+    /**
+	 * @author  Soumya
+	 * @since   2017-01-22
+     * 
+     * @return
+     */
     List<CustomerDetails> findAll();
  
+    /**
+	 * @author  Soumya
+	 * @since   2017-01-22
+     * 
+     * @param id
+     * @return
+     */
     Optional<CustomerDetails> findOne(String id);
  
+    /**
+	 * @author  Soumya
+	 * @since   2017-01-22
+     * 
+     * @param saved
+     * @return
+     */
     CustomerDetails save(CustomerDetails saved);
 }
