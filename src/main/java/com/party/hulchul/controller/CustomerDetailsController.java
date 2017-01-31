e6c3d3b5c447a73e3d4396a55b457d39fa5a82ca
Soumya <sbasak>
2017-01-30 20:17:41 -0700

    Controller rest method has been added for the custom search operation

--- src/main/java/com/party/hulchul/controller/CustomerDetailsController.java
+++ src/main/java/com/party/hulchul/controller/CustomerDetailsController.java
@@ -3,6 +3,7 @@
  */
 package com.party.hulchul.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import javax.ws.rs.Consumes;
 
@@ -11,10 +12,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.party.hulchul.model.CustomerDetails;
 import com.party.hulchul.service.CustomerDetailsService;
+import com.party.hulchul.util.JsonParserUtil;
 
 /**
  * @author RKottak
@@ -23,31 +26,39 @@ import com.party.hulchul.service.CustomerDetailsService;
 @RequestMapping("/v1/customer")
 @RestController
 public class CustomerDetailsController {
-    @Autowired
-    CustomerDetailsService service;
-    
-	@RequestMapping(value = "/getCustomer", method = RequestMethod.GET, params = {"customerId"})
+	@Autowired
+	CustomerDetailsService service;
+
+	@RequestMapping(value = "/getCustomer", method = RequestMethod.GET, params = { "customerId" })
 	@Consumes("application/json")
-	public CustomerDetails getCustomerDetails(@RequestParam(value="customerId") String customerId){
-        return service.findById(customerId);
-    }
-	
-	@RequestMapping(value = "/deleteCustomer", method = RequestMethod.GET, params = {"customerId"})
+	public CustomerDetails getCustomerDetails(@RequestParam(value = "customerId") String customerId) {
+		return service.findById(customerId);
+	}
+
+	@RequestMapping(value = "/deleteCustomer", method = RequestMethod.GET, params = { "customerId" })
 	@Consumes("application/json")
-	public boolean deleteCustomerDetails(@RequestParam(value="customerId") String customerId){
-        return service.delete(customerId);
-    }
-	
-    /**
-	 * @author  Soumya
-	 * @since   2017-01-22
-     * 
-     * @param todoEntry
-     * @return
-     */
-    @RequestMapping(value = "/createCustomer", method = RequestMethod.POST)
-    @Consumes("application/json")
-    CustomerDetails createCustomerDetails(@RequestBody @Valid CustomerDetails todoEntry) {
-        return service.create(todoEntry);
-    }
+	public boolean deleteCustomerDetails(@RequestParam(value = "customerId") String customerId) {
+		return service.delete(customerId);
+	}
+
+	/**
+	 * @author Soumya
+	 * @since 2017-01-22
+	 * 
+	 * @param todoEntry
+	 * @return
+	 */
+	@RequestMapping(value = "/createCustomer", method = RequestMethod.POST)
+	@Consumes("application/json")
+	CustomerDetails createCustomerDetails(@RequestBody @Valid CustomerDetails todoEntry) {
+		return service.create(todoEntry);
+	}
+
+	@RequestMapping(value = "/search", method = RequestMethod.GET)
+	public @ResponseBody String searchCustomerDetails(@RequestParam String search, HttpServletRequest request) {
+
+		return JsonParserUtil.toString(service.searchCustomerByCustomStringDaoImpl(search));
+
+	}
+
 }
