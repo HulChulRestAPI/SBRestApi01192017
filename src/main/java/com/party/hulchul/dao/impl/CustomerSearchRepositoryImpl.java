38e81700884e351a58fcb7f89738cd5c66e68f45
Soumya <sbasak>
2017-01-30 20:22:00 -0700

    Unified implementation class has been added for the custom operations related to the Customer entity

---
+++ src/main/java/com/party/hulchul/dao/impl/CustomerSearchRepositoryImpl.java
@@ -0,0 +1,41 @@
+package com.party.hulchul.dao.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Repository;
+
+import com.party.hulchul.dao.CustomerSearchRepository;
+import com.party.hulchul.model.Customer;
+
+/**
+* <h1>The responsibility of this class is to 
+* provide a unified implementation for the custom operations related to the Customer</h1>
+*
+* @author  Soumya
+* @version 1.0
+* @since   2017-01-28
+*/
+
+@Repository
+public class CustomerSearchRepositoryImpl implements CustomerSearchRepository {
+
+	@Autowired
+	MongoTemplate mongoTemplate;
+
+	@Override
+	public List<Customer> searchCustomerByCustomString(String searchString) {
+		
+		return mongoTemplate.find(Query.query(new Criteria().
+									orOperator(Criteria.where("firstName").regex(searchString,"i"),
+												Criteria.where("lastName").regex("searchString", "i"),
+												Criteria.where("emailAddress").regex("searchString", "i"))
+									), Customer.class);
+		
+		
+	}
+
+}
